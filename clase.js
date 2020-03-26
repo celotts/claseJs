23e064d4107f1d8eec16b3e35bbe8ec064b48b63
= <celotts@gmail.com>
2020-03-25 22:07:03 -0300

    .

--- clase.js
+++ clase.js
@@ -11,8 +11,48 @@ Personas.prototype.saludar = function () {
 let name = 'Carlos';
 let age = 47;
 
+const xy = [1, 2, 5, 9, 3, 7, 6]
+
+if (xy.includes(90)) {
+    console.log('Existe 9');
+} else {
+    console.log('No existe 9');
+}
+
 obj = { name, age };
 console.log(obj);
+
+const data = {
+    frontend: 'Carlos',
+    backEnd: 'Nestor',
+    design: 'Julio'
+}
+
+// Convierte en objeto en array
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+// Convierte un objeto en un arreglo string
+const data1 = {
+    frontend: 'Carlos',
+    backEnd: 'Nestor',
+    design: 'Julio'
+}
+
+const values = Object.values(data1);
+console.log(values);
+console.log(values.length);
+
+const objx = {
+    name1: 'Carlos',
+    age1: 47,
+    country1: 'VEN'
+};
+
+// Muestra los objeto age1 and country
+let { name1, ...all } = objx;
+console.log(name1, all);
 Personas.prototype.soyAlto = function () {
     return this.altura > 1.8
 }
